b593b63a3af797f21f89c00abf3f7e12dcff03b0
kilmerluizale@gmail.com <kilmerluizale@gmail.com>
2016-08-26 14:35:37 -0300

    cleaning project

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,16 +18,6 @@ class QuotesController < ApplicationController
     @quotes = Quote.order('id DESC').all
   end
 
-  # DELETE /quotes/1
-  # DELETE /quotes/1.json
-  def destroy
-    @quote.destroy
-    respond_to do |format|
-      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
       params.require(:quote).permit(:content, :userSender, :myth, :quoteDate)
